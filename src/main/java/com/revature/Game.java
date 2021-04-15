e4572c8ccee276473ddce743db4d6de1d459d1b6
mattdo <mattdo290@gmail.com>
2021-04-15 18:32:54 -0500

    implementation for game is ready for classes

--- src/main/java/com/revature/Game.java
+++ src/main/java/com/revature/Game.java
@@ -1,6 +1,5 @@
 package com.revature;
 
-import java.io.InputStream;
 import java.util.Scanner;
 
 /**
@@ -36,14 +35,83 @@ public class Game {
         String playerName = scanner.nextLine();
         Player player = new Player(playerName);
         do {
-
+            if (winCount < 2) {
+                beginCreatureEncounter(player);
+            } else {
+                beginBossEncounter(player);
+            }
         } while (winCount < 3 && lostCount < 5);
         endGame();
     }
 
-    private boolean evaluateRound(String playerSign, String creatureSign) {
-        switch () {
+    private void beginCreatureEncounter(Player player) {
+        //TODO: Generate a creature randomly
+        Creature creature = new Creature();
+        beginBattle(player, creature);
+    }
 
+
+    private void beginBossEncounter(Player player) {
+        //TODO: Generate a boss creature
+        Creature creature = new Creature();
+        beginBattle(player, creature);
+    }
+
+    private void beginBattle(Player player, Creature creature) {
+        int roundsWon = 0;
+        int roundsTotal = 0;
+        outputToConsole(String.format("%s encounters a %s. It looks like %s. Prepare to battle!", player.name, creature.name, creature.description));
+        do {
+            //TODO: Call player to throw a sign
+            player.sign = "scissors";
+            //TODO: Call creature to throw a sign
+            creature.sign = "paper";
+            try {
+                boolean result = evaluateRound(player.sign, creature.sign);
+                if (result) {
+                    roundsWon += 1;
+                }
+                roundsTotal += 1;
+                //TODO: Change generic Exception to TIE exception
+            } catch (Exception e) {
+                outputToConsole("You and the creature resulted in a tie. A new round will start. Enter any key to continue.");
+                Scanner scanner = new Scanner(System.in);
+                scanner.nextLine();
+            }
+        } while (roundsTotal < 3);
+
+        if (roundsWon >= 2) {
+            winCount ++;
+        } else {
+            lostCount += 1;
+            winCount = 0;
+        }
+    }
+
+    private boolean evaluateRound(String playerSign, String creatureSign) throws Exception {
+        switch (playerSign.toUpperCase()) {
+            case "P":
+                return isPlayerWon(creatureSign, "R", "S");
+            case "R":
+                return isPlayerWon(creatureSign, "S", "P");
+            case "S":
+                return isPlayerWon(creatureSign, "P", "R");
+            default:
+                //TODO: handle input other than r, p, and s
+                return false;
+        }
+    }
+
+    private boolean isPlayerWon(String creatureSign, String winningSign, String losingSign) throws Exception {
+        if (creatureSign.equalsIgnoreCase(winningSign)) {
+            return true;
+        } else {
+            if (creatureSign.equalsIgnoreCase(losingSign)) {
+                return false;
+            } else {
+                //TODO: Throw a "TIE" exception
+                throw new Exception();
+            }
         }
     }
 
@@ -70,10 +138,12 @@ public class Game {
         Player(String name) { }
     }
 
-    private abstract class Creature {
+    private class Creature {
         private String name;
         private String description;
         private String sign;
+
+        public Creature() { }
 
         public Creature(String name, String description, String sign) {
             this.name = name;
